9a233d512c06722bb9192be421d4f955186254b4
Samuel <Sommosgaming@gmail.com>
2017-11-02 02:01:28 +0000

    Add files via upload

--- RSAEncryption.java
+++ RSAEncryption.java
@@ -1,32 +1,61 @@
-import java.lang.Math;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 public class RSAEncryption {
 	public static void main(String[] args){
+//STEP ONE AND TWO
 	int p=61;
 	int q=53;
-	int n=p*q;
+//STEP THREE
+	Integer pq=p*q;
+//STEP FOUR
 	int pTotient=p-1;
 	int qTotient=q-1;
+//STEP FIVE
 	int finalTotient=pTotient*qTotient;
-	int e=17;
+//STEP SIX AND SEVEN
+	int e=0;
+	if((p>17)||(q>17)){
+		e=17;
+	}else{
+		e=7;
+	}
+//STEP EIGHT AND NINE
 	int d=0;
 	int count=0;
-	while ((e*d%3120)!=1){
+	while ((e*d%finalTotient)!=1){
 		d=d+1;
 		count=count+1;
 	}
-	System.out.println("n = "+n);
+//STEP TEN
+	int ed=e*d;
+	System.out.println("p = "+p);
+	System.out.println("q = "+q);
+	System.out.println("pq = "+pq);
 	System.out.println("final totient = "+finalTotient);
-	System.out.println("calculation of e*d = "+e*d);
+	System.out.println("calculation of ed = "+ed);
 	System.out.println("d = "+d);
+//ENCRYPTION
 	int message=123;
-//	public key
+//PUBLIC KEY
 	Double c=java.lang.Math.pow(message,e);
-	System.out.println(c);
 	BigInteger f=new BigDecimal(c).toBigInteger();
-	System.out.println(f);
-	int ca=intC+15;
-//private key
+	System.out.println("our full public exponent = "+f);
+	BigInteger g=BigInteger.valueOf(pq.intValue());
+	BigInteger h=f.mod(g);
+	System.out.println("our encrypted message = "+h);
+//PRIVATE KEY
+	int i=h.intValue();
+//Another way of doing power
+//	Double j=java.lang.Math.pow(i,d);
+//	System.out.println(j);
+	//1 way of doing power
+//	BigInteger k=new BigDecimal(j).toBigInteger();
+	System.out.println("our encrypted message as an int data type =  "+i);
+	//another way of doing power
+	BigInteger l=h.pow(d).add(BigInteger.ONE);
+	System.out.println(l);
+	//Could store it in an array and bring each integer out one by one, but the run and compile time would be ridiculous.
+	BigInteger m=l.mod(g);
+	System.out.println(m);
 	}	
 }
---
+++ Sieve.java
@@ -0,0 +1,34 @@
+//import java.security.SecureRandom;
+public class Sieve{
+//	randomFunction(){
+//	SecureRandom random=new SecureRandom();
+//	byte[] values=new byte[20];
+//	random.nextBytes(values);
+//	System.out.println("random = "+random);
+//	}
+	public static void main(String[] args){
+		while(true){
+			long p=(long)(Math.random()*(1000000000-2)+2);
+	        if(primeTest(p)){
+	        System.out.println("prime p:"+p);
+	        break;
+	        }
+		}
+	    while(true){
+	        long q=(long)(Math.random()*(1000000000-2)+2);
+	        if(primeTest(q)){
+	        	System.out.println("prime q:"+q);
+	        	break;
+	        }
+	    }
+	}
+	private static boolean primeTest(long n){
+		int i;
+		for(i=2;i<=Math.sqrt(n);i++){
+			if(n%i==0){
+				return false;
+			}
+		}
+	       return true;
+	}
+}
