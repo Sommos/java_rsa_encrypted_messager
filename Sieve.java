9edb480314d6d063b0a6117e023244c14cf7b4e3
Samuel <Sommosgaming@gmail.com>
2017-12-03 21:20:38 +0000

    Update Sieve.java

--- Sieve.java
+++ Sieve.java
@@ -1,24 +1,11 @@
+package default_package;
 public class Sieve{
 	public static void main(String[] args){
-		while(true){
-			long p=(long)(Math.random()*(1000000000-2)+2);
-	        	if(primeTest(p)){
-	        		System.out.println("prime p:"+p);
-	       			break;
-	        	}
-		}
-	    	while(true){
-	        	long q=(long)(Math.random()*(1000000000-2)+2);
-	        	if(primeTest(q)){
-	        		System.out.println("prime q:"+q);
-	        		break;
-	        	}
-	    	}
 	}
 	public int randPrimeBetween(int start, int end){
 		int prime;
 		do{
-			prime = (int)(start+Math.random()*end);
+			prime=(int)(start+Math.random()*end);
 		}while(!primeTest((long)prime));			
 		return prime;
 	}
