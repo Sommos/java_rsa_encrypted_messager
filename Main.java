df344778a6e19f362d1e7a93ce93b42a91dde934
Samuel <Sommosgaming@gmail.com>
2017-12-04 22:10:37 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,30 @@
 package default_package;
+
 import java.math.BigInteger;
 import javax.swing.JOptionPane;
+
 public class Main {
 	public static void main(String[] args) {
+		String userInput = JOptionPane.showInputDialog("Please enter your message");
+		int message = Integer.parseInt(userInput);
+		Main.println("Original User Message = " + message + ".");
+		// WHEN NEW MESSAGE IS ENTERED BY USER DO { //
 		RSAEncryption rsa = new RSAEncryption();
 		int p = rsa.getPrimeP();
 		int q = rsa.getPrimeQ();
 		while(p == q){
 			p = rsa.getPrimeP();
 		}
-		// WHEN NEW MESSAGE IS ENTERED BY USER DO { //
-		String userInput = JOptionPane.showInputDialog("Please enter your message");
-		int message = Integer.parseInt(userInput);
-		BigInteger encryptedMessage = EncryptionDecryptionMethods.returnEncryptedMessage(message, p, q);
-		BigInteger decryptedMessage = EncryptionDecryptionMethods.returnDecryptedMessage(encryptedMessage, p, q);
+		EncryptionDecryptionMethods encDecMethods = new EncryptionDecryptionMethods();
+		BigInteger encryptedMessage = encDecMethods.returnEncryptedMessage(message, p, q);
+		BigInteger decryptedMessage = encDecMethods.returnDecryptedMessage(encryptedMessage, p, q);
 		// } THIS //
 	}
 	// METHODS FOR MAIN CLASS // 
-	static void println(Object x){
+	static void println(Object x) {
 		System.out.println(x);
+	}
+	static void errorPrintln(Object x) {
+		System.err.println(x);
 	}
 }
