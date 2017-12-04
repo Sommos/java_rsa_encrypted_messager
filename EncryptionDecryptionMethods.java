ef31013c5faa9db7f6bfa8ef6f5234c161e5285b
Samuel <Sommosgaming@gmail.com>
2017-12-04 22:10:19 +0000

    Update EncryptionDecryptionMethods.java

--- EncryptionDecryptionMethods.java
+++ EncryptionDecryptionMethods.java
@@ -1,22 +1,24 @@
 package default_package;
+
 import java.math.BigInteger;
+
 public class EncryptionDecryptionMethods {
-	public static BigInteger returnEncryptedMessage(int message, int p, int q){
+	public BigInteger returnEncryptedMessage(int message, int p, int q){
 		RSAEncryption rsa = new RSAEncryption();
 		BigInteger bigIntegerMessage = rsa.castBigInteger(message);
-		BigInteger bigIntegerE = rsa.castBigInteger(rsa.getE(p,q));
-		BigInteger bigIntegerPQ = rsa.castBigInteger(rsa.getPQ(p,q));
+		BigInteger bigIntegerE = rsa.castBigInteger(rsa.getE());
+		BigInteger bigIntegerPQ = rsa.castBigInteger(rsa.getPQ(p, q));
 		BigInteger encryptedMessage = rsa.powerMod(bigIntegerMessage, bigIntegerE, bigIntegerPQ);
-		Main.println("Encrypted Message = "+ encryptedMessage);
+		Main.println("Encrypted Message = " + encryptedMessage + ".");
 		return encryptedMessage;
 	}
-	public static BigInteger returnDecryptedMessage(BigInteger encryptedMessage, int p, int q){
+	public BigInteger returnDecryptedMessage(BigInteger encryptedMessage, int p, int q){
 		RSAEncryption rsa = new RSAEncryption();
 		BigInteger bigIntegerPQ = rsa.castBigInteger(rsa.getPQ(p,q));
 		int lcm = (int) rsa.lcm(rsa.getPrimePTotient(p),rsa.getPrimeQTotient(q));
-		BigInteger bigIntegerD = rsa.castBigInteger(rsa.getD(rsa.getE(p, q), lcm));
+		BigInteger bigIntegerD = rsa.castBigInteger(rsa.getD(rsa.getE(), lcm));
 		BigInteger decryptedMessage = rsa.powerMod(encryptedMessage, bigIntegerD, bigIntegerPQ);
-		Main.println("Decrypted Message = "+decryptedMessage);
-		return decryptedMessage;
+		Main.println("Decrypted Message = " + decryptedMessage + ".");
+		return decryptedMessage;	
 	}
 }
