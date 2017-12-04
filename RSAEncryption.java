19826506344876af7e7f53c1f8f957ae89b50f08
Samuel <Sommosgaming@gmail.com>
2017-12-04 22:10:06 +0000

    Update RSAEncryption.java

--- RSAEncryption.java
+++ RSAEncryption.java
@@ -1,31 +1,47 @@
 package default_package;
+
 import java.math.BigInteger;
+
 public class RSAEncryption {
-	public int getPrimeP() {
+	
+	// THIS GETS THE FIRST PRIME (P) //
+	protected int getPrimeP() {
 		Sieve sieve = new Sieve();
-		int p = sieve.randPrimeBetween(50, 99);
+		int p = sieve.randPrimeBetween(50, 60);
 		return p;
 	}
-	public int getPrimeQ() {
+	
+	// THIS GETS THE SECOND PRIME (Q) //
+	protected int getPrimeQ() {
 		Sieve sieve = new Sieve();
-		int q = sieve.randPrimeBetween(50, 99);
+		int q = sieve.randPrimeBetween(50, 60);
 		return q;
 	}
+	
+	// THIS CALCULATES THE MULTIPLICATION OF P AND Q //
 	public Integer getPQ(int p, int q) {
 		Integer pq = p * q;
 		return pq;
 	}
+	
+	// THIS GETS THE TOTIENT OF PRIME P //
 	public int getPrimePTotient(int p) {
 		int pTotient = p-1;
 		return pTotient;
 	}
+	
+	// THIS GETS THE TOTIENT OF PRIME Q //
 	public int getPrimeQTotient(int q) {
 		int qTotient = q-1;
 		return qTotient;
 	}
+	
+	// THIS CALCULATES LCM //
 	public long lcm(long a, long b) {
 		return a * (b / gcd(a,b));
 	}
+	
+	// THIS CALCULATES GCD //
 	public long gcd(long a, long b) {
 		while(b != 0){
 			long temp = b;
@@ -34,15 +50,25 @@ public class RSAEncryption {
 		}
 		return a;
 	}
-	public int getE(int p, int q) {
-		int e = 0;
+	
+	// THIS CALCULATES E FOR THE PROGRAM //
+	protected int getE() {
+		int e = 65537;
+		return e;
+// LEAVE THIS ALONE FOR THE MOMENT - MAY NEED IT LATER //
+/*		int e = 0;
+		for (e=0; e<lcm; e++){
+			
+		} 
 		if((p > 17) || (q > 17)) {
-			e = 17;
+			e = 257;
 		}else{
 			e = 7;
 		}
-		return e;
+		return e; */		
 	}
+	
+	// THIS CALCULATES D FOR THE PROGRAM //
 	public int getD(int e, int lcm){
 		int d = 0;
 		int count = 0;
@@ -51,7 +77,7 @@ public class RSAEncryption {
 			count = count + 1;
 			if((e * d % lcm) == 1){
 				if(d > 1000000){
-					Main.println("Error Code : 0");
+					Main.errorPrintln("Error Code : 0");
 					Main.println(d);
 					System.exit(0);
 				}
@@ -60,10 +86,14 @@ public class RSAEncryption {
 		d = (d < 0) ? d * - 1 : d;
 		return d;	
 	}
+	
+	// THIS METHOD CASTS AN INT TO A BIGINTEGER DATA TYPE (FOR EASE OF USE) //
 	public BigInteger castBigInteger(int number){
-		BigInteger x = new BigInteger(""+number);
+		BigInteger x = new BigInteger("" + number);
 		return x;
 	}
+	
+	// THIS METHOD POWERS AND MODS GIVEN VALUES //
 	public BigInteger powerMod(BigInteger message, BigInteger e, BigInteger pq){
 		BigInteger powerMod = message.modPow(e,
 				pq);
