791dafed88f85ae4926dfcac76aff7bff9ceb250
Samuel <Sommosgaming@gmail.com>
2017-12-15 21:36:06 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,50 +1,75 @@
 package default_package;
 
-import java.math.BigInteger;
 import java.util.Arrays;
+import java.math.BigInteger;
+import java.io.IOException;
 
 import javax.swing.JOptionPane;
 
 public class Main {
 	public static void main(String[] args) {
 		String userInput = JOptionPane.showInputDialog("Please enter your message");
-		try {
-		int message = Integer.parseInt(userInput);
-		Main.println("Original User Message = " + message + ".");
+//		try {
+//		int message = Integer.parseInt(userInput);
+//		println("Original User Message = " + userInput + ".");
 		// WHEN USER PRESSES SEND BUTTON DO { //
-		byte[] messageBytes = userInput.getBytes();
-		RSAEncryption rsa = new RSAEncryption();
-		int p = rsa.getPrimeP();
-		int q = rsa.getPrimeQ();
-		while(p == q) {
-			p = rsa.getPrimeP();
-		}
-			// TESTING PRINTS //
-			println(Arrays.toString(messageBytes));
-			println("p = " + p);
-			println("q = " + q);
-			println("pq = " + rsa.getPQ(p, q));
-			int lcm = (int) rsa.lcm(rsa.getPrimePTotient(p),rsa.getPrimeQTotient(q));
-			println("d = " + rsa.getD(rsa.getE(),lcm,p,q));
-
-			EncryptionDecryptionMethods encDecMethods = new EncryptionDecryptionMethods();
-			BigInteger encryptedMessage = encDecMethods.returnEncryptedMessage(message, p, q);
-			BigInteger decryptedMessage = encDecMethods.returnDecryptedMessage(encryptedMessage, p, q);
-			exit(0);
-			// } THIS //
-		} catch (Exception e){
-			println("ERROR CODE : 0\n");
-			println("PLEASE, RESTART THE PROGRAM. A LOGIC FAULT HAS OCCURED.");
-			exit(0);
-		}
+//		byte[] messageBytes = userInput.getBytes();
+        println("Encrypting String: " + userInput);
+        println("String in Bytes: " + bytesToString(userInput.getBytes()));
+        RSAEncryption rsa = new RSAEncryption();
+        BigInteger p = rsa.getPrimeP();
+        BigInteger q = rsa.getPrimeQ();
+        EncryptionDecryptionMethods encDecMeth = new EncryptionDecryptionMethods();
+        byte[] encrypted = encDecMeth.returnEncryptedMessage(userInput.getBytes(), p, q);
+        byte[] decrypted = encDecMeth.returnDecryptedMessage(encrypted,rsa.getD(rsa.getE(), p, q), rsa.getPQ(p, q));
+        println("Decrypting Bytes: " + bytesToString(decrypted));
+        println("Decrypted String: " + new String(decrypted));
+//		RSAEncryption rsa = new RSAEncryption();
+//		BigInteger p = rsa.getPrimeP();
+//		BigInteger q = rsa.getPrimeQ();
+//		while(p == q) {
+//			p = rsa.getPrimeP();
+//		}
+//			// TESTING PRINTS //
+//			println(Arrays.toString(messageBytes));
+//			println("p = " + p);
+//			println("q = " + q);
+//			println("pq = " + rsa.getPQ(p, q));
+//			BigInteger lcm = rsa.lcm(rsa.getPrimePTotient(p),rsa.getPrimeQTotient(q));
+//			println("d = " + rsa.getD(rsa.getE(),lcm,p,q));
+//
+//			EncryptionDecryptionMethods encDecMethods = new EncryptionDecryptionMethods();
+//			BigInteger encryptedMessage = encDecMethods.returnEncryptedMessage(message, p, q);
+//			BigInteger decryptedMessage = encDecMethods.returnDecryptedMessage(encryptedMessage, p, q);
+//			exit(0);
+//			// } THIS //
+//		} catch (Exception e) {
+//			println("ERROR CODE : 0\n");
+//			println("PLEASE, RESTART THE PROGRAM. A LOGIC FAULT HAS OCCURED.");
+//			exit(0);
+//		}
 	}
-	// METHODS FOR MAIN CLASS // 
+	// METHODS FOR MAIN CLASS //
+	// METHOD TO CONVERT BYTES TO A STRING //
+    private static String bytesToString(byte[] encrypted) {
+        String test = "";
+        for (byte b : encrypted) {
+            test += Byte.toString(b);
+        }
+        return test;
+    }
+    
+    // METHOD TO PRINT OUT A LINE IN WHITE // 
 	static void println(Object x) {
 		System.out.println(x);
 	}
+	
+	// METHOD TO PRINT OUT AN ERROR IN RED //
 	static void errorPrintln(Object x) {
 		System.err.println(x);
 	}
+	
+	// METHOD TO EXIT THE PROGRAM // 
 	static void exit(int x) {
 		System.exit(x);
 	}
