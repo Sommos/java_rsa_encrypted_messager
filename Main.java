2a3c542be3af9b3a76b8ba48440cf2aa88ff9e52
Samuel <Sommosgaming@gmail.com>
2017-12-15 20:36:20 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,42 @@
 package default_package;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 
 public class Main {
 	public static void main(String[] args) {
 		String userInput = JOptionPane.showInputDialog("Please enter your message");
+		try {
 		int message = Integer.parseInt(userInput);
 		Main.println("Original User Message = " + message + ".");
-		// WHEN NEW MESSAGE IS ENTERED BY USER DO { //
+		// WHEN USER PRESSES SEND BUTTON DO { //
+		byte[] messageBytes = userInput.getBytes();
 		RSAEncryption rsa = new RSAEncryption();
 		int p = rsa.getPrimeP();
 		int q = rsa.getPrimeQ();
-		while(p == q){
+		while(p == q) {
 			p = rsa.getPrimeP();
 		}
-		EncryptionDecryptionMethods encDecMethods = new EncryptionDecryptionMethods();
-		BigInteger encryptedMessage = encDecMethods.returnEncryptedMessage(message, p, q);
-		BigInteger decryptedMessage = encDecMethods.returnDecryptedMessage(encryptedMessage, p, q);
-		// } THIS //
+			// TESTING PRINTS //
+			println(Arrays.toString(messageBytes));
+			println("p = " + p);
+			println("q = " + q);
+			println("pq = " + rsa.getPQ(p, q));
+			int lcm = (int) rsa.lcm(rsa.getPrimePTotient(p),rsa.getPrimeQTotient(q));
+			println("d = " + rsa.getD(rsa.getE(),lcm,p,q));
+
+			EncryptionDecryptionMethods encDecMethods = new EncryptionDecryptionMethods();
+			BigInteger encryptedMessage = encDecMethods.returnEncryptedMessage(message, p, q);
+			BigInteger decryptedMessage = encDecMethods.returnDecryptedMessage(encryptedMessage, p, q);
+			exit(0);
+			// } THIS //
+		} catch (Exception e){
+			println("ERROR CODE : 0\n");
+			println("PLEASE, RESTART THE PROGRAM. A LOGIC FAULT HAS OCCURED.");
+			exit(0);
+		}
 	}
 	// METHODS FOR MAIN CLASS // 
 	static void println(Object x) {
@@ -26,5 +44,8 @@ public class Main {
 	}
 	static void errorPrintln(Object x) {
 		System.err.println(x);
+	}
+	static void exit(int x) {
+		System.exit(x);
 	}
 }
