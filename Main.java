5a3b1d3609e9c479675936fc0b52d681d19d0295
Samuel <Sommosgaming@gmail.com>
2017-12-16 04:20:13 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
-package default_package;
+package encryption;
 
 import java.math.BigInteger;
 import javax.swing.JOptionPane;
 
 public class Main {
 	public static void main(String[] args) {
-		String userInput = JOptionPane.showInputDialog("Please enter your message");
-//		try {
-//		int message = Integer.parseInt(userInput);
-//		println("Original User Message = " + userInput + ".");
-		// WHEN USER PRESSES SEND BUTTON DO { //
-//		byte[] messageBytes = userInput.getBytes();
-        println("String: " + userInput);
+		// TRACKS TIME TO RUN IN MS //
+		long start = System.currentTimeMillis();
+
+		// GET USER INPUT MESSAGE //
+		String userInput = JOptionPane.showInputDialog("Please enter the message you want encrypted");
+		
+		// TESTING STRINGS //
+		println("String = " + userInput);
         RSAEncryption rsa = new RSAEncryption();
         BigInteger p = rsa.getPrimeP();
         println("Prime P = " + p);
@@ -21,57 +22,59 @@ public class Main {
         BigInteger e = rsa.getE();
         BigInteger pq = rsa.getPQ(p, q);
         byte[] encrypted = encDecMeth.returnEncryptedMessage(userInput.getBytes(), p, q, e, pq);
-        byte[] decrypted = encDecMeth.returnDecryptedMessage(encrypted,rsa.getD(e, p, q), pq);
         // MAY CHANGE THIS BACK TO DECRYPTED IF BYTE VALUE FAILS //
-        println("Byte Value: " + bytesToString(encrypted) + ".");
-        println("Decrypted String: " + new String(decrypted) + ".");
-//		RSAEncryption rsa = new RSAEncryption();
-//		BigInteger p = rsa.getPrimeP();
-//		BigInteger q = rsa.getPrimeQ();
-//		while(p == q) {
-//			p = rsa.getPrimeP();
-//		}
-//			// TESTING PRINTS //
-//			println(Arrays.toString(messageBytes));
-//			println("p = " + p);
-//			println("q = " + q);
-//			println("pq = " + rsa.getPQ(p, q));
-//			BigInteger lcm = rsa.lcm(rsa.getPrimePTotient(p),rsa.getPrimeQTotient(q));
-//			println("d = " + rsa.getD(rsa.getE(),lcm,p,q));
-//
-//			EncryptionDecryptionMethods encDecMethods = new EncryptionDecryptionMethods();
-//			BigInteger encryptedMessage = encDecMethods.returnEncryptedMessage(message, p, q);
-//			BigInteger decryptedMessage = encDecMethods.returnDecryptedMessage(encryptedMessage, p, q);
-//			exit(0);
-//			// } THIS //
-//		} catch (Exception e) {
-//			println("ERROR CODE : 0\n");
-//			println("PLEASE, RESTART THE PROGRAM. A LOGIC FAULT HAS OCCURED.");
-//			exit(0);
-//		}
+        println("Byte Value = " + encDecMeth.bytesToString(encrypted));
+        byte[] decrypted = encDecMeth.returnDecryptedMessage(encrypted,rsa.getD(e, p, q), pq);
+        String finalDecryptedString = new String(decrypted);
+        println("Decrypted String = " + finalDecryptedString);	
+	
+/*   	try {
+    		int message = Integer.parseInt(userInput);
+    		println("Original User Message = " + userInput + ".");
+    		// WHEN USER PRESSES SEND BUTTON DO { //
+    		byte[] messageBytes = userInput.getBytes();	
+		RSAEncryption rsa = new RSAEncryption();
+		BigInteger p = rsa.getPrimeP();
+	
+		+BigInteger q = rsa.getPrimeQ();
+		while(p == q) {
+			p = rsa.getPrimeP();
+		}
+			// TESTING PRINTS //
+			println(Arrays.toString(messageBytes));
+			println("p = " + p);
+			println("q = " + q);
+			println("pq = " + rsa.getPQ(p, q));
+			BigInteger lcm = rsa.lcm(rsa.getPrimePTotient(p),rsa.getPrimeQTotient(q));
+			println("d = " + rsa.getD(rsa.getE(),lcm,p,q));
+
+			EncryptionDecryptionMethods encDecMethods = new EncryptionDecryptionMethods();
+			BigInteger encryptedMessage = encDecMethods.returnEncryptedMessage(message, p, q);
+			BigInteger decryptedMessage = encDecMethods.returnDecryptedMessage(encryptedMessage, p, q);
+			exit(0);
+			// } THIS //
+		} catch (Exception e) {
+			println("ERROR CODE : 0\n");
+			println("PLEASE, RESTART THE PROGRAM. A LOGIC FAULT HAS OCCURED.");
+			exit(0);
+		}	*/
+        println("Time to run = " + (System.currentTimeMillis() - start) + "ms.");
+        println("Program run without any issues - GG.\n\nEXIT - FINAL TRANSMISSION.");
 	}
 	// METHODS FOR MAIN CLASS //
-	// METHOD TO CONVERT BYTES TO A STRING //
-    private static String bytesToString(byte[] encrypted) {
-        String test = "";
-        for (byte b : encrypted) {
-            test += Byte.toString(b);
-        }
-        return test;
-    }
     
     // METHOD TO PRINT OUT A LINE IN WHITE // 
-	static void println(Object x) {
+	public static void println(Object x) {
 		System.out.println(x);
 	}
 	
 	// METHOD TO PRINT OUT AN ERROR IN RED //
-	static void errorPrintln(Object x) {
+	public static void errorPrintln(Object x) {
 		System.err.println(x);
 	}
 	
 	// METHOD TO EXIT THE PROGRAM // 
-	static void exit(int x) {
+	public static void exit(int x) {
 		System.exit(x);
 	}
 }
