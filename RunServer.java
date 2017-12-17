dae1004e588b6961a56ced451518527ed7d11daf
Samuel <Sommosgaming@gmail.com>
2017-12-17 18:40:20 +0000

    Update and rename ServerTest.java to RunServer.java

--- RunServer.java
+++ RunServer.java
@@ -2,7 +2,7 @@ package encryption;
 
 import javax.swing.JFrame;
 
-public class ServerTest {
+public class RunServer {
 	public static void main(String[] args) {
 		Server server = new Server();
 		server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
