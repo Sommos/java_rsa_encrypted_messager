76dde39ef5475f977136d0fa4b6dc671be1547d5
Samuel <Sommosgaming@gmail.com>
2017-12-17 18:31:47 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,66 +1,48 @@
 package encryption;
 
 import java.math.BigInteger;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+// DON'T GET MAD AT THIS CODE - IT WAS WRITTEN BY AN IDIOT. YOU. //
 public class Main {
 	public static void main(String[] args) {
-		// TRACKS TIME TO RUN IN MS //
 		long start = System.currentTimeMillis();
-
-		// GET USER INPUT MESSAGE //
-		String userInput = JOptionPane.showInputDialog("Please enter the message you want encrypted");
 		
-		// TESTING STRINGS //
-		println("String = " + userInput);
-        RSAEncryption rsa = new RSAEncryption();
-        BigInteger p = rsa.getPrimeP();
+		String userInput = "This is a test. Nothing but a test. I swear ;)";
+		String userInputBitLengthString = JOptionPane.showInputDialog("Please enter the bit length of the encryption keys.");
+		int userInputBitLengthInt = Integer.parseInt(userInputBitLengthString);
+		RSAEncryption rsa = new RSAEncryption();
+	
+		println("Original string = " + userInput);
+        BigInteger p = rsa.getPrimeP(userInputBitLengthInt);
+        BigInteger q = rsa.getPrimeQ(userInputBitLengthInt);
+   
+        println("\nThese are the original RSA encryption keys (using " + userInputBitLengthInt + " encryption):\n");
+        println("-----BEGIN RSA PRIVATE KEY-----");
         println("Prime P = " + p);
-        BigInteger q = rsa.getPrimeQ();
-        println("Prime Q = " + q);
+        println("Prime Q = " + q + "");
+        println("-----END RSA PRIVATE KEY-----\n");
+   
         EncryptionDecryptionMethods encDecMeth = new EncryptionDecryptionMethods();
         BigInteger e = rsa.getE();
         BigInteger pq = rsa.getPQ(p, q);
         byte[] encrypted = encDecMeth.returnEncryptedMessage(userInput.getBytes(), p, q, e, pq);
-        // MAY CHANGE THIS BACK TO DECRYPTED IF BYTE VALUE FAILS //
-        println("Byte Value = " + encDecMeth.bytesToString(encrypted));
+        
+        // MAY CHANGE THIS BACK TO DECRYPTED IF BYTE VALUE FAILS BUT WE SHOULD BE GOOD //
+        println("Byte Value (Encrypted) = " + encDecMeth.bytesToString(encrypted) + "\n");
+        
         byte[] decrypted = encDecMeth.returnDecryptedMessage(encrypted,rsa.getD(e, p, q), pq);
         String finalDecryptedString = new String(decrypted);
         println("Decrypted String = " + finalDecryptedString);	
-	
-/*   	try {
-    		int message = Integer.parseInt(userInput);
-    		println("Original User Message = " + userInput + ".");
-    		// WHEN USER PRESSES SEND BUTTON DO { //
-    		byte[] messageBytes = userInput.getBytes();	
-		RSAEncryption rsa = new RSAEncryption();
-		BigInteger p = rsa.getPrimeP();
-	
-		+BigInteger q = rsa.getPrimeQ();
-		while(p == q) {
-			p = rsa.getPrimeP();
-		}
-			// TESTING PRINTS //
-			println(Arrays.toString(messageBytes));
-			println("p = " + p);
-			println("q = " + q);
-			println("pq = " + rsa.getPQ(p, q));
-			BigInteger lcm = rsa.lcm(rsa.getPrimePTotient(p),rsa.getPrimeQTotient(q));
-			println("d = " + rsa.getD(rsa.getE(),lcm,p,q));
-
-			EncryptionDecryptionMethods encDecMethods = new EncryptionDecryptionMethods();
-			BigInteger encryptedMessage = encDecMethods.returnEncryptedMessage(message, p, q);
-			BigInteger decryptedMessage = encDecMethods.returnDecryptedMessage(encryptedMessage, p, q);
-			exit(0);
-			// } THIS //
-		} catch (Exception e) {
-			println("ERROR CODE : 0\n");
-			println("PLEASE, RESTART THE PROGRAM. A LOGIC FAULT HAS OCCURED.");
-			exit(0);
-		}	*/
-        println("Time to run = " + (System.currentTimeMillis() - start) + "ms.");
-        println("Program run without any issues - GG.\n\nEXIT - FINAL TRANSMISSION.");
+		println("\nTime to run = " + (System.currentTimeMillis() - start) + "ms.");
+        println("\nProgram ran without any issues - GG WP. EXIT CODE: 0.\nEXIT - FINAL TRANSMISSION. (M83 - INTRO).");
+		
+        Client client = new Client("127.0.0.1");
+		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		client.startRunning();
 	}
+	
 	// METHODS FOR MAIN CLASS //
     
     // METHOD TO PRINT OUT A LINE IN WHITE // 
