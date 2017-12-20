c745f5e31eeb1f6d3bc108e05a4a34572a3d77d1
Samuel <Sommosgaming@gmail.com>
2017-12-20 00:51:20 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,88 +10,86 @@ public class Main {
 		// Saves the current system time to a variable // 
 		long start = System.currentTimeMillis();
 		
+		demonstration();
+		// Prints the time to run out to the console //
+		println("\nTime to run = " + (System.currentTimeMillis() - start) + "ms.");
+		
+		client();
+	}
+	// Methods for the Main class //
+ 
+	// Method for showing a demonstration of the encryption algorithm //
+	private static void demonstration() {
 		// This is the start of the basic RSA encryption demonstration //
 		println("--------------------| Demonstration Program - Build 0.1.57 |--------------------");
-		
-		// Gets the message from the user //
-		String userInput = JOptionPane.showInputDialog("Enter your message");
-		
-		// Gets the bit length of the keys that the user wants to use //
-		String userInputBitLengthString = JOptionPane.showInputDialog("Enter the bit length of the encryption keys (Binary Multiples)");
-		
-		// Parses the resulting String to an int //
-		int userInputBitLengthInt = Integer.parseInt(userInputBitLengthString);
-		
 		// Instantiates the RSAEncryption class //
 		RSAEncryption rsa = new RSAEncryption();
 		
-		// Instantiates the EncryptionDecryptionMethods class //
-        EncryptionDecryptionMethods encDecMeth = new EncryptionDecryptionMethods();
+		// Gets the message from the user //
+		String userInput = JOptionPane.showInputDialog("Enter your message");
+		// Prompts the user for their desired bit length of keys //
+		rsa.setBitLength();
 	
 		println("Original String = " + userInput);
 		
-		// Generates the two primes keys that will be used for our encryption //
-        BigInteger p = rsa.getPrimeP(userInputBitLengthInt);
-        BigInteger q = rsa.getPrimeQ(userInputBitLengthInt);
-   
+		// Sets the values of the two prime keys that will be used for our encryption //
+        rsa.setPrimeP(rsa.getBitLength());
+        rsa.setPrimeQ(rsa.getBitLength());
+        // Gets the values of primes and assigns memory addresses //
+        BigInteger p = rsa.getPrimeP();
+        BigInteger q = rsa.getPrimeQ();   
+        // Sets the value of prime p totient //
+        rsa.setPrimePTotient(p);        
+        // Sets the value of prime q totient //
+        rsa.setPrimeQTotient(q);        
+        // Sets the value of e //
+        rsa.setE();        
+        // Sets the value of d //
+        rsa.setD(rsa.getE(), p, q);
+        // Gets the value of e //
+        BigInteger e = rsa.getE();       
+        // Sets the value of pq //
+        rsa.setPQ(p, q);    
+        // Gets the value of pq from our entered values of p and q (Our original keys) //
+        BigInteger pq = rsa.getPQ(); 
+       
         // Prints the generated keys to the console as proof of concept //
-        println("\nThese are the original RSA encryption keys (using " + userInputBitLengthInt + " encryption):\n");
+        println("\nThese are the original RSA encryption keys (using " + rsa.getBitLength() + " encryption):\n");
         println("-----BEGIN RSA PRIVATE KEY-----");
         println("Prime P = " + p);
         println("Prime Q = " + q);
-        println("-----END RSA PRIVATE KEY-----\n");
-        
-        // Gets the value of e //
-        BigInteger e = rsa.getE();
-        
-        // Gets the value of pq from our entered values of p and q (Our original keys) //
-        BigInteger pq = rsa.getPQ(p, q);
-        
+        println("-----END RSA PRIVATE KEY-----\n");        
+       
         // Takes the values generated and the users original message, encrypts it and returns it as a byte array //
-        byte[] encrypted = encDecMeth.returnEncryptedMessage(userInput.getBytes(), p, q, e, pq);
-        
+        byte[] encrypted = rsa.returnEncryptedMessage(userInput.getBytes(), p, q, e, pq);        
         // May change this back to decrypted if byte values fail but we should be good //
-        println("Byte Value (ENCRYPTED) = " + encDecMeth.bytesToString(encrypted));
+        println("Byte Value (ENCRYPTED) = " + rsa.bytesToString(encrypted));        
         
         // Decrypts the values from the keys given (p and q) //
-        byte[] decrypted = encDecMeth.returnDecryptedMessage(encrypted,rsa.getD(e, p, q), pq);
-        
+        byte[] decrypted = rsa.returnDecryptedMessage(encrypted, rsa.getD(), pq);        
         // Prints out the simplistic byte value to the console - May be useful later //
-        println("Byte Value (DECRYPTED) = " + encDecMeth.bytesToString(decrypted) + "\n");
+        println("Byte Value (DECRYPTED) = " + rsa.bytesToString(decrypted) + "\n");        
         
         // Converts the byte array to a String //
-        String finalDecryptedString = new String(decrypted);
-        
+        String finalDecryptedString = new String(decrypted);        
         // Prints the decrypted String out to the console //
         println("Decrypted String = " + finalDecryptedString);
-        
-        // AWEIGHBWQIEGWQEIUQGEIWUQGEIQUGE //
-        
-
-        // AUDBHASIOUDHWEQOIEHWQOIEHWOEIWE //
-        
-        // Prints the time to run out to the console //
-		println("\nTime to run = " + (System.currentTimeMillis() - start) + "ms.");
-		
 		// Prints exit code out to the console - More for error checking than anything else //
-        println("\nDemonstration ran without any issues - GG WP. EXIT CODE: 0.\nEXIT - FINAL TRANSMISSION. (M83 - INTRO).");
-		
-        // That is the end of the demonstration of my RSA encryption //
-        
-        // Start of the actual instant messaging application //
-        
-        // Instantiates the Client class, with the local IP address (This should be changed when in actual use) //
-        Client client = new Client("127.0.0.1");
-        
-        // Sets up the default close situation (Which is when the JFrame is closed by the user) //
+        println("\nDemonstration ran without any issues - GG WP.\nEXIT - FINAL TRANSMISSION. (M83 - INTRO).");
+	}
+	
+	// Method that connects to the server and launches a client JFrame //
+	private static void client() {
+		// Instantiates the Client class, with the local IP address (This should be changed when in actual use) //
+		Client client = new Client("127.0.0.1");
+    
+		// Sets up the default close situation (Which is when the JFrame is closed by the user) //
 		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+	
 		// Starts the running of the Client class as a thread //
 		client.startRunning();
 	}
 	
-	// Methods for the Main class //
-    
     // Method to print to the console in white text // 
 	public static void println(Object x) {
 		System.out.println(x);
