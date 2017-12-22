84fff230f6e20b42c1165e46f5251af8a0ba9a24
Samuel <Sommosgaming@gmail.com>
2017-12-22 17:32:31 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 package encryption;
 
-import java.math.BigInteger;
 import javax.swing.JFrame;
+import java.math.BigInteger;
 import javax.swing.JOptionPane;
 
 // Don't get mad at this code - it was written by an idiot //
@@ -9,28 +9,28 @@ public class Main {
 	public static void main(String[] args) {
 		// Saves the current system time to a variable // 
 		long start = System.currentTimeMillis();
-		
+
 		demonstration();
 		// Prints the time to run out to the console //
 		println("\nTime to run = " + (System.currentTimeMillis() - start) + "ms.");
 		
-		client();
+//		client();
 	}
 	// Methods for the Main class //
  
 	// Method for showing a demonstration of the encryption algorithm //
 	private static void demonstration() {
 		// This is the start of the basic RSA encryption demonstration //
-		println("--------------------| Demonstration Program - Build 0.1.57 |--------------------");
+	println("|--------------------| Demonstration Program - Build 0.1.79 |--------------------|");
 		// Instantiates the RSAEncryption class //
 		RSAEncryption rsa = new RSAEncryption();
 		
 		// Gets the message from the user //
-		String userInput = JOptionPane.showInputDialog("Enter your message");
+		rsa.setUserInput();
 		// Prompts the user for their desired bit length of keys //
 		rsa.setBitLength();
 	
-		println("Original String = " + userInput);
+		println("Original user input = " + rsa.getUserInput());
 		
 		// Sets the values of the two prime keys that will be used for our encryption //
         rsa.setPrimeP(rsa.getBitLength());
@@ -52,27 +52,37 @@ public class Main {
         // Prints the generated keys to the console as proof of concept //
         println("\nThese are the original RSA encryption keys (using " + rsa.getBitLength() + " encryption):\n");
         println("-----BEGIN RSA PRIVATE KEY-----");
-        println("Prime P = " + p);
-        println("Prime Q = " + q);
-        println("-----END RSA PRIVATE KEY-----\n");        
+        println("Prime p = " + p);
+        println("Prime q = " + q);
+        println("-----END RSA PRIVATE KEY-----\n");
         
         // Takes the values generated and the users original message, encrypts it and returns it as a byte array //
-        byte[] encrypted = rsa.returnEncryptedMessage(userInput.getBytes(), p, q, rsa.getE(), rsa.getPQ());        
+        byte[] encrypted = rsa.returnEncryptedMessage(rsa.getUserInput().getBytes(), p, q, rsa.getE(), rsa.getPQ());        
         // May change this back to decrypted if byte values fail but we should be good //
-        println("Byte Value (ENCRYPTED) = " + rsa.bytesToString(encrypted));        
+        println("Byte value of user input (ENCRYPTED) = " + rsa.bytesToString(encrypted));        
         
         // Decrypts the values from the keys given (p and q) //
         byte[] decrypted = rsa.returnDecryptedMessage(encrypted, rsa.getD(), rsa.getPQ());        
         // Prints out the simplistic byte value to the console - May be useful later //
-        println("Byte Value (DECRYPTED) = " + rsa.bytesToString(decrypted) + "\n");        
+        println("Byte value of user input (DECRYPTED) = " + rsa.bytesToString(decrypted) + "\n");        
         
         // Converts the byte array to a String //
         String finalDecryptedString = new String(decrypted);        
         // Prints the decrypted String out to the console //
-        println("Decrypted String = " + finalDecryptedString);
+        println("Decrypted user input = " + finalDecryptedString);
+        // Checks if original input is equal to the decrypted value //
+        if (finalDecryptedString.equals(rsa.getUserInput())) {
+        	println("Equal check : TRUE.");
+        } else if (!finalDecryptedString.equals(rsa.getUserInput())) {
+        	println("Equal check : FALSE.");
+        } else {
+        	println("ERROR CODE : 0");
+        	exit(0);
+        } 
 		// Prints exit code out to the console - More for error checking than anything else //
-        println("\nDemonstration ran without any issues - GG WP.\nEXIT - FINAL TRANSMISSION. (M83 - INTRO).");
+        println("\nDemonstration ran without any issues - GG WP.\n   _\n _(.)< EXIT - FINAL TRANSMISSION. (M83 - INTRO).\n/___)");
 	}
+	// Methods for the Main class //
 	
 	// Method that connects to the server and launches a client JFrame //
 	private static void client() {
