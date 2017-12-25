55d8bc9109c2f8faed345075b4fce245b38f4fc7
Samuel <Sommosgaming@gmail.com>
2017-12-25 05:50:40 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,14 +8,36 @@ public class Main {
 	public static void main(String[] args) {
 		// Saves the current system time to a variable // 
 		long start = System.currentTimeMillis();
-
-		demonstration();
+		
+		assignValues();
+		
+//		demonstration();
 		// Prints the time to run out to the console //
 		println("\nTime to run = " + (System.currentTimeMillis() - start) + "ms.");
 		
 		client();
 	}
 	// Methods for the Main class //
+	
+	// Method for giving values to each variable needed for the encryption algorithm //
+	private static void assignValues() {
+		RSAEncryption rsa = new RSAEncryption();
+		// Prompts the user for their desired bit length of keys //
+		rsa.setBitLengthDefault();
+		// Sets the values of the two prime keys that will be used for our encryption //
+	    rsa.setPrimeP(rsa.getBitLength());
+	    rsa.setPrimeQ(rsa.getBitLength());
+	    // Sets the value of prime p totient //
+		rsa.setPrimePTotient(rsa.getPrimeP());        
+		// Sets the value of prime q totient //
+		rsa.setPrimeQTotient(rsa.getPrimeQ());        
+		// Sets the value of e //
+		rsa.setE();        
+		// Sets the value of d //
+		rsa.setD(rsa.getE(), rsa.getPrimeP(), rsa.getPrimeQ());     
+		// Sets the value of pq //
+		rsa.setPQ(rsa.getPrimeP(), rsa.getPrimeQ());    
+	}
 	
 	// Method for showing a demonstration of the encryption algorithm //
 	private static void demonstration() {
