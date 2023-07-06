b2e5a7184ccba621640b444122e557deb24a9a7c
Samuel <Sommosgaming@gmail.com>
2023-07-06 14:40:49 +0100

    Update BasicMethods.java

--- BasicMethods.java
+++ BasicMethods.java
@@ -4,9 +4,11 @@ public class BasicMethods {
 	public void println(Object x) {
 		System.out.println(x);
 	}
+	
 	public void errorln(Object x) {
 		System.err.println(x);
 	}
+	
 	public void exit() {
 		System.exit(0);
 	}
